1001ddf9dfac1c55ab87fadf25f9c84236199083
vineet11pareek <vineet00pareek@gmail.com>
2025-12-09 16:43:52 +0100

    update rest API's method to get the path variable

--- src/main/java/com/challenge/DeviceManager/controller/DeviceController.java
+++ src/main/java/com/challenge/DeviceManager/controller/DeviceController.java
@@ -21,31 +21,31 @@ public class DeviceController {
     }
 
     @PutMapping("/update/{deviceId}")
-    public String updateDevice(@RequestParam String deviceId){
+    public String updateDevice(@PathVariable String deviceId){
         System.out.println(deviceId);
         return "its update device";
     }
 
     @GetMapping("/getById/{deviceId}")
-    public String getDeviceById(@RequestParam String deviceId){
+    public String getDeviceById(@PathVariable String deviceId){
         System.out.println(deviceId);
         return "its get device by ID";
     }
 
     @GetMapping("/getByBrand/{brand}")
-    public String getDeviceByBrand(@RequestParam String state){
-        System.out.println(state);
+    public String getDeviceByBrand(@PathVariable String brand){
+        System.out.println(brand);
         return "its get device by Brand";
     }
 
     @GetMapping("/getByState/{state}")
-    public String getDeviceByState(@RequestParam String brand){
-        System.out.println(brand);
+    public String getDeviceByState(@PathVariable String state){
+        System.out.println(state);
         return "its get device by state";
     }
 
     @DeleteMapping("/delete/{deviceId}")
-    public String deleteDevice(@RequestParam String deviceId){
+    public String deleteDevice(@PathVariable String deviceId){
         System.out.println(deviceId);
         return "its delete device by ID";
     }
